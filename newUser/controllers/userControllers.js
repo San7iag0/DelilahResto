cc66765edf926f460821a170a9077852b13fdfdb
santiago <ttsbpn@gmail.com>
2020-05-05 21:43:46 -0500

    admin users

--- newUser/controllers/userControllers.js
+++ newUser/controllers/userControllers.js
@@ -1,8 +1,31 @@
 const userRoutes = require('../routes/userRoutes');
 
-exports.signup = (req, res, next) => {
+app.user('/api/suff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
-}
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 20).then(
+        (hash) => {
+            const user = new User  ({
+                email: req.body.email,
+                password: hash
+            });
+            user.save().then(
+                () => {
+                    res.status(201).json({
+                        message: 'user added'
+                    });
+                }
+            ).catch(
+                (error) => {
+                    res.status(500).json({
+                        error:error          
+                    })
+                }
+            );
+        }
+    );
+};
 
 exports.login = (req, res, next) => {
     
--- newUser/routes/userRoutes.js
+++ newUser/routes/userRoutes.js
@@ -7,3 +7,5 @@ router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
+
+// code for the new Admins user login and sign up
