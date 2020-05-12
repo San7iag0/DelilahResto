a6777a379d7af900078ee7af6025fac8b9bec23f
santiago <ttsbpn@gmail.com>
2020-05-11 22:13:47 -0500

    finish first endpoint

--- newUser.js
+++ newUser.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const server = express();
 server.use(express.json())
-const email;
-const pass;
+
+// const Email = email;
+// const Pass = pass;
 
 //Users 
-const users = [
-  {name: "Pepito", email: "pepito@correo.com"}
-]
+const users = [{
+  name: "", 
+  email: ""
+}]
 
 /*
   {
@@ -28,46 +30,32 @@ function agregarLog(req, res, next){
 }
 server.use(agregarLog);
 
-server.post('/users/add', verificar, (req, res) => {
+
+server.post('/users/add', (req, res) => {
     const usuario = req.body;
     users.push(usuario)
     console.log(users);
     res.json(req.body);
 });
 
-//Verified if the Email is not repeat 
-const verificar = (req, res, next) => {
-  const { email } = req.body;
-  console.log(email);
-  users.forEach( users => {
-
-      console.log(user.email);
-    if (user.email === email) {
-        
-      res.json({error: 'Email repetido'})
-    } else {
-      next();
-    }
-  });
-}
-
 
 // validated the user 
+function validateUser(req, res, next){
+  const {email, pass} = req.body;
+  if (email == admin.email || pass == admin.pass){
+    res.status(400)
+    .json('User or password incorrect');
+  }else {
+    console.log('sucessful validation');
+    next();
+  }
+} 
 
-// function validarUsuario(req, res, next){
-//   const {email, pass} = req.body;
-//   if (email !== admin.email || pass !== admin.pass){
-//     res.status(400)
-//     .json('usuario o clave incorrecta');
-//   }else {
-//     console.log('validacion exitosa');
-//     next();
-//   }
-// } 
+server.post('/admin', validateUser, (req, res) => {
+    res.json('access success')
+});
 
-// server.post('/admin', validarUsuario, (req, res) => {
-//     res.json('Acceso correcto')
-// });
+
 
 //see list of users 
 server.get('/users', (req, res) => {
@@ -75,7 +63,10 @@ server.get('/users', (req, res) => {
 })
 
 
-server.listen(8080, () => {
-    console.log('servidor corriendo');
+server.listen(3000, () => {
+    console.log('server Runing');
 })
 
+
+
+//npm i express mysql2 sequelize 
