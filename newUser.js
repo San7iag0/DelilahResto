75cbe686cd7ae221744349b9ecb9bb4b803dda57
santiago <ttsbpn@gmail.com>
2020-05-12 23:07:01 -0500

    users

--- newUser.js
+++ newUser.js
@@ -1,24 +1,38 @@
 const express = require('express');
 const server = express();
+const jsonWTok = require("jsonwebtoken");
+
 server.use(express.json())
+
 
 // const Email = email;
 // const Pass = pass;
 
-//Users 
-const users = [{
+const users = [
+  {
+    "userName": "Pepita",
+    "fullName": "smith",
+    "email": "pep@email.com",
+    "phone": 123456,
+    "Address": "123 calle falsa,sprinfield",
+    "password": "123abc",
+  },  
+{
   name: "pedro", 
   email: "pedro@pp.com"
 }]
 
+const products = [{}]
+
+
 /*
   {
-    "UserName": "Pepita",
-    "fullName": "smith"
-    "Email": "pep@email.com"
-    "phone": 123456
-    "Address": 
-    "password": 
+    "userName": "Pepita",
+    "fullName": "smith",
+    "email": "pep@email.com",
+    "phone": 123456,
+    "Address":  ,
+    "password":  ,
   }
 */
 
@@ -32,10 +46,21 @@ server.use(agregarLog);
 
 
 server.post('/users/add', (req, res) => {
-    const usuario = req.body;
-    users.push(usuario)
+    // const user = req.body;
+    //const {userName, fullName, email, phone, address, password} = req.body;
+    // if (userName.lenght !== 0 || password.lenght !== 0){
+    //   return res.status(400)
+    //   .then(
+    //     users.push(user)
+    //     res.status(200).json({userName, fullName, email, phone, address, password });
+    //   )  
+    // }
+    
+    // else { 
+    //   res.status(500).json({ error: "Error" });
+    // }
     console.log(users);
-    res.json(req.body);
+    //res.json(req.body);
 });
 
 
@@ -55,11 +80,14 @@ server.post('/admin', validateUser, (req, res) => {
     res.json('access success')
 });
 
-
+server.get("/products/get", (req, res) => {
+  res.json(products)
+})
 
 //see list of users 
 server.get('/users', (req, res) => {
   res.json(users)
+  res.status(200);
 })
 
 
