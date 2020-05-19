4c7f7cae9440b1e42bcd781608ba11f4bc7e147b
santiago <ttsbpn@gmail.com>
2020-05-18 19:21:25 -0500

    order proj

---
+++ API/Users/userModel.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
+
+const UserSchema = new Schema({ 
+    userName: String,
+    fullName: String,
+    email: {type: String, unique: true},
+    phone: Number, 
+    address: {type: String, unique: true},
+    password: {type: String, select: false},    
+    admin: Boolean
+})
+// created password and hash to incrypted 
+UserSchema.pre('save', (next) => {
+    let user = this
+    if (!user.ismodified('password')) return next()
+
+    bcrypt.genSalt(10, (err, salt) => { 
+        if (err) return next()
+        
+        bcrypt.hash(user.password, salt, null, (err, hash) => {
+            if(err) return next(err)
+
+            user.password = hash
+            next()
+        })
+    })
+})
+
+module.exports = mongoose.model('User', UserSchema)
+
---
+++ Database/dataBase.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const mysql = require("mysql2");
+const server = express();
+const sequelize = require("sequelize");
+
+
